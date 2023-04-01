91876b751187c1e52b6613e995c21bd48417f4f6
Matvienko Misha <matvienko.mk@phystech.edu>
2023-04-01 18:02:06 +0300

    new algorithm

--- problem_lc.c
+++ problem_lc.c
@@ -2,86 +2,87 @@
 #include <stdlib.h>
 
 struct page {
+    struct page *next;
+    struct page *before;
     int index;
     char data[64];
 };
 
-struct page *create_list(int index)
+struct page *create_list(int index, struct page *next, struct page *before)
 {
-    struct page *p = (struct page *) malloc(sizeof(struct page));
-    p->data[0] = 'A';           //here can read page's data
-    p->index = index;
+    struct page *page = (struct page *) malloc(sizeof(struct page));
+    page->next = next;
+    page->before = before;
+    page->data[0] = 'A';     //here can read page's data
+    page->index = index;
 
-    return p;
+    return page;
 }
 
-
-void change_pages(struct page **hash_t, int size_cash, int index_find_el)
+void change_last_el(struct page **hash_t, int last, int el)
 {
-    int i = 0;
-
-    struct page *find_el;
-
-    find_el = hash_t[index_find_el];
-
-    for (i = index_find_el; i < size_cash - 1; ++i) {
-        hash_t[i] = hash_t[i + 1];
-    }
-
-    hash_t[i] = find_el;
-
+    last = hash_t[last]->before->index;    //penultimate el becomes last
+    free(hash_t[last]->next->data);
+    hash_t[hash_t[last]->next->index] = NULL;
 }
 
-void add_new_page(struct page **hash_t, int size_cash, int el)
+void change_head_el(struct page **hash_t, int head, int el)
 {
-    int i = 0;
-
-    for (i = 0; i < size_cash - 1; ++i)
-        hash_t[i] = hash_t[i + 1];
-    hash_t[i] = create_list(el);
-}
-
-int find_in_hash_t(struct page **hash_t, int size_cash, int el)
-{
-    int i = 0;
-
-    for (i = size_cash - 1; i >= 0; --i) {
-        if (hash_t[i]->index == el) {
-            return i;
-        }
-    }
-
-    return -1;
-
+    hash_t[el] = create_list(el, hash_t[head], NULL);
+    hash_t[head]->before = hash_t[el];
+    head = el;
 }
 
 int count_cash_hit(struct page **hash_t, int count_el, int size_cash)
 {
     int i = 0;
     int el = 0;
+    int last = 0;
+    int head = 0;
     int index_find_el = 0;
     int cash_hit = 0;
 
-    for (i = 0; i < count_el; ++i) {
+    //create first el in list
+    scanf("%d ", &el);
+    head = el;
+    last = el;
+    hash_t[el] = create_list(el, NULL, NULL);
+
+    //fill cash
+    for (i = 1; i < size_cash; ++i) {
         scanf("%d ", &el);
-        index_find_el = find_in_hash_t(hash_t, size_cash, el);  // -1 <=> not find, j (>= 0) - number of list
-        if (index_find_el == -1) {
-            add_new_page(hash_t, size_cash, el);
+        change_head_el(hash_t, head, el);
+    }
+
+    for (i = size_cash; i < count_el; ++i) {
+        scanf("%d ", &el);
+        if (hash_t[el] == NULL) {
+            change_last_el(hash_t, last, el);
+            //hash_t[last]->next = NULL;
+            change_head_el(hash_t, head, el);
         } else {
             ++cash_hit;
-            change_pages(hash_t, size_cash, index_find_el);
+            if (hash_t[el]->before == NULL) {       //if first in list
+                continue;
+            } else if (hash_t[el]->next == NULL) {  //if last in list
+                hash_t[hash_t[el]->before->index]->next = NULL;
+                change_head_el(hash_t, head, el);
+            } else {
+                hash_t[hash_t[el]->before->index]->next = hash_t[hash_t[el]->next->index];
+                change_head_el(hash_t, head, el);
+            }
         }
     }
 
     return cash_hit;
 }
 
-void create_hash_t(struct page **hash_t, int size_cash)
+void create_hash_t(struct page **hash_t, int count_el)
 {
     int i = 0;
 
-    for (i = 0; i < size_cash; ++i) {
-        hash_t[i] = create_list(0);
+    for (i = 0; i < count_el; ++i) {
+        hash_t[i] = NULL;
     }
 }
 
@@ -97,7 +98,7 @@ int main()
     scanf("%d ", &count_el);
 
     hash_t = (struct page **) calloc(size_cash, sizeof(struct page *));
-    create_hash_t(hash_t, size_cash);
+    create_hash_t(hash_t, count_el);
 
     printf("%d\n", count_cash_hit(hash_t, count_el, size_cash));
 
